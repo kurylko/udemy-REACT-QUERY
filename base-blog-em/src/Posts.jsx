6b93d6f1bf5a53b529f47ce9197c0f71e9b5a763
kurylko <kurylko@lcs-schlieben.de>
2025-12-24 12:03:25 +0100

    prefetch

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"], //key is always an array
-    queryFn: fetchPosts, // function that returns a promise to fetch a data
+  const queryClient = useQueryClient();
+  
+// Prefetching next page, data stored in cache
+  useEffect(() => {
+    if(currentPage < maxPostPage) {   
+    const nextPage = currentPage + 1;
+    // Pre-fetch the next page
+    if (currentPage < maxPostPage) {    
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }
+  }, [currentPage, queryClient]);
+
+  const { data, isError, error, isLoading, isFetching } = useQuery({
+    queryKey: ["posts", currentPage], //key is always an array
+    queryFn: () => fetchPosts(currentPage), // function that returns a promise to fetch a data
     // passing the function by reference, no args, no params
     staleTime: 2000, // data considered fresh for 2 seconds - then needs to be refetched
   });
@@ -24,6 +40,10 @@ export function Posts() {
 // early return for error state
   if(isError) {
     return <h2>{error.message}</h2>;
+  }
+
+  if(isFetching) {
+    console.log("Background fetching posts...");
   }
 
   console.log("Posts data:", data);
@@ -42,11 +62,15 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+        disabled={currentPage <= 1} 
+        onClick={() => setCurrentPage(currentPage - 1)}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button 
+        disabled={currentPage >= maxPostPage} 
+        onClick={() => setCurrentPage(currentPage + 1)}>
           Next page
         </button>
       </div>
