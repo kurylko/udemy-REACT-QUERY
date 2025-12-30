ddcf9dc973223a880b2339a419ce8449aea4721f
kurylko <kurylko@lcs-schlieben.de>
2025-12-30 14:40:42 +0100

    tests

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,25 +1,42 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { render as RtlRender } from "@testing-library/react";
 import { PropsWithChildren, ReactElement } from "react";
 import { MemoryRouter } from "react-router-dom";
 
 // ** FOR TESTING CUSTOM HOOKS ** //
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
-// export const createQueryClientWrapper = () => {
-//   const queryClient = generateQueryClient();
-//   return ({ children }: PropsWithChildren) => (
-//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-//   );
-// };
+export const createQueryClientWrapper = () => {
+  const queryClient = generateQueryClient();
+  return ({ children }: PropsWithChildren) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+// helper to generate a QueryClient with desired default options for tests
+export const generateQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false, // disable retries to fail tests fast
+        staleTime: Infinity, // prevent refetching on mount
+      },
+    },
+  });
+};
 
 // reference: https://testing-library.com/docs/react-testing-library/setup#custom-render
-function customRender(ui: ReactElement) {
+function customRender(ui: ReactElement, client?: QueryClient) {
+  const queryClient = client ?? generateQueryClient();
+
   return RtlRender(
     <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
       <MemoryRouter>{ui}</MemoryRouter>
+      </QueryClientProvider>
     </ChakraProvider>
   );
-}
+};
 
 // re-export everything
 // eslint-disable-next-line react-refresh/only-export-components
